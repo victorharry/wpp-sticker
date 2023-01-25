53c02ebfd5cf6c741d42469a514c2dca904f55f9
Leonardo T. Fernandes <leo.80@protonmail.com>
2023-01-25 00:48:35 -0300

    feat: add background removal

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { Client, MessageMedia } = require('whatsapp-web.js')
 const qrcode = require('qrcode-terminal')
 const axios = require('axios')
 const client = new Client({})
+const { removeBackgroundFromImageBase64 } = require('remove.bg')
+require('dotenv').config()
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true})
@@ -18,8 +20,7 @@ client.on('ready', () => {
  */
 client.on('message_create', msg => {
     const command = msg.body.split(' ')[0];
-    // Cola seu número onde tem o 84848484, sem o 9
-    const sender = msg.from.includes("84848484") ? msg.to : msg.from
+    const sender = msg.from.includes(process.env.PHONE_NUMBER_WITHOUT_FIRST_DIGIT) ? msg.to : msg.from
     if (command === "/sticker")  generateSticker(msg, sender)
 });
 
@@ -28,22 +29,46 @@ client.initialize();
 const generateSticker = async (msg, sender) => {
     if(msg.type === "image") {
         try {
-            const { data } = await msg.downloadMedia()
-            const image = await new MessageMedia("image/jpeg", data, "image.jpg")
+            let { data: image } = await msg.downloadMedia()
+
+            if (msg.body.includes('--transparent')) {
+                image = await removeBackground(image)
+            }
+
+            image = await new MessageMedia("image/jpeg", data, "image.jpg")
             await client.sendMessage(sender, image, { sendMediaAsSticker: true })
         } catch(e) {
             msg.reply("❌ Erro ao processar imagem")
         }
     } else {
         try {
-
-            const url = msg.body.substring(msg.body.indexOf(" ")).trim()
+            const url = msg.body.split(' ').pop();
             const { data } = await axios.get(url, {responseType: 'arraybuffer'})
-            const returnedB64 = Buffer.from(data).toString('base64');
+            let returnedB64 = Buffer.from(data).toString('base64');
+
+            if (msg.body.includes('--transparent')) {
+                returnedB64 = await removeBackground(returnedB64)   
+            }
+
             const image = await new MessageMedia("image/jpeg", returnedB64, "image.jpg")
             await client.sendMessage(sender, image, { sendMediaAsSticker: true })
         } catch(e) {
             msg.reply("❌ Não foi possível gerar um sticker com esse link")
         }
+    }
+}
+
+const removeBackground = async (image) => {
+    try  {
+        const result = await removeBackgroundFromImageBase64({
+            base64img: image,
+            apiKey: process.env.REMOVE_BG_API_KEY,
+            size: 'regular',
+            type: 'auto',
+        })
+    
+        return result.base64img
+    } catch (e) {
+        console.log(JSON.stringify(e))
     }
 }
